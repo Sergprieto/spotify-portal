a8407aca252936eb71e58ee365e5ff5871168ad4
Sergio Prieto <sergprieto@live.com>
2021-09-23 19:47:19 -0700

    Format using Prettier-Standard extension

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,39 +1,36 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react'
 import styles from './App.module.css'
 import { wave, getAllWaves, connectWallet } from '../utils/contractConnection'
 
-
 const App = () => {
-
-  const [currAccount, setCurrentAccount] = useState('');
-  const [message, setMessage] = useState('');
-  const [allWaves, setAllWaves] = useState<any[]>([]);
-
+  const [currAccount, setCurrentAccount] = useState('')
+  const [message, setMessage] = useState('')
+  const [allWaves, setAllWaves] = useState<any[]>([])
 
   const connectWalletHandler = async () => {
-    const walletArr = await connectWallet();
+    const walletArr = await connectWallet()
     if (walletArr) {
-      setCurrentAccount(walletArr);
-      loadInitWaves();
+      setCurrentAccount(walletArr)
+      loadInitWaves()
     }
   }
 
   const loadInitWaves = async () => {
-      const initialWaves = await getAllWaves();
-      setAllWaves(initialWaves);
+    const initialWaves = await getAllWaves()
+    setAllWaves(initialWaves)
   }
 
-
   useEffect(() => {
-    document.title = "Sergio's Spotify Portal";
-  }, []);
+    document.title = "Sergio's Spotify Portal"
+  }, [])
 
   const showConnectForm = currAccount ? (
     <form>
-      <input type="text" 
-        id="message" 
-        placeholder="Enter a message!" 
-        value={message} 
+      <input
+        type='text'
+        id='message'
+        placeholder='Enter a message!'
+        value={message}
         onChange={e => setMessage(e.target.value)}
       />
     </form>
@@ -42,23 +39,24 @@ const App = () => {
       Connect Wallet
     </button>
   )
-  
+
   return (
     <div className={styles.mainContainer}>
-
       <div className={styles.dataContainer}>
-        <div className={styles.header}>
-        👋 Hey there!
-        </div>
+        <div className={styles.header}>👋 Hey there!</div>
 
         <div className={styles.bio}>
-        <h1>
-        I am Sergio and I am in my last semester of my Software Engineering degree! 
-        </h1>
-        <br/>
-        For now, all you can do is wave at me and maybe win some sick fake ethereum. <br/>But I do enjoy the messages 🙂
-        <br/>
-        Soon you will be able to connect your etherium wallet and link me your favorite spotify music!
+          <h1>
+            I am Sergio and I am in my last semester of my Software Engineering
+            degree!
+          </h1>
+          <br />
+          For now, all you can do is wave at me and maybe win some sick fake
+          ethereum. <br />
+          But I do enjoy the messages 🙂
+          <br />
+          Soon you will be able to connect your etherium wallet and link me your
+          favorite spotify music!
         </div>
 
         <button className={styles.waveButton} onClick={() => wave(message)}>
@@ -67,19 +65,25 @@ const App = () => {
 
         {showConnectForm}
 
-
         {allWaves.map((wave, index) => {
           return (
-            <div key={index} style={{backgroundColor: "OldLace", marginTop: "16px", padding: "8px"}}> 
+            <div
+              key={index}
+              style={{
+                backgroundColor: 'OldLace',
+                marginTop: '16px',
+                padding: '8px'
+              }}
+            >
               <div>Address: {wave.address}</div>
               <div>Time: {wave.timestamp.toString()} </div>
               <div>Message: {wave.message} </div>
             </div>
           )
-        })} 
+        })}
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/utils/contractConnection.ts
+++ src/utils/contractConnection.ts
@@ -1,55 +1,58 @@
 import * as waveArtifacts from './WavePortal.json'
-import { ethers, Contract } from 'ethers';
+import { ethers, Contract } from 'ethers'
 
 export interface WaveMessage {
-  address: string,
-  timestamp: Date,
-  message: string,
+  address: string
+  timestamp: Date
+  message: string
 }
 
-const contractAddress: any = "0xc87974C4e3C86b0b2D1d15c2DB7d8c097ba34Eb4";
-const contractABI = waveArtifacts.abi;
+const contractAddress: any = '0xc87974C4e3C86b0b2D1d15c2DB7d8c097ba34Eb4'
+const contractABI = waveArtifacts.abi
 
 export const connectWallet = async (): Promise<string> => {
-  const { ethereum }: any = window;
+  const { ethereum }: any = window
   if (!ethereum) {
-    console.log('Get metamask first to connect a wallet');
-    return "";
+    console.log('Get metamask first to connect a wallet')
+    return ''
   }
 
-    const accounts: string[] = await ethereum.request({ method: 'eth_requestAccounts' })
-    console.log('Connected', accounts[0]);
-    return accounts[0];
-};
+  const accounts: string[] = await ethereum.request({
+    method: 'eth_requestAccounts'
+  })
+  console.log('Connected', accounts[0])
+  return accounts[0]
+}
 
-const getPortalContract = async(): Promise<Contract> => {
-    const provider = new ethers.providers.Web3Provider((window as any).ethereum);
-    const signer = provider.getSigner();
-    return new ethers.Contract(contractAddress, contractABI, signer);
-
+const getPortalContract = async (): Promise<Contract> => {
+  const provider = new ethers.providers.Web3Provider((window as any).ethereum)
+  const signer = provider.getSigner()
+  return new ethers.Contract(contractAddress, contractABI, signer)
 }
 
 export const wave = async (message: string) => {
-  const wavePortalContract = await getPortalContract();
+  const wavePortalContract = await getPortalContract()
 
-  let count = await wavePortalContract.getTotalWaves();
-  console.log('Retrieved total wave count...', count.toNumber());
+  let count = await wavePortalContract.getTotalWaves()
+  console.log('Retrieved total wave count...', count.toNumber())
 
-  const waveTxn: any = await wavePortalContract.wave(message, { gasLimit: 300000 })
-  console.log('Mining...', waveTxn.hash);
-  await waveTxn.wait();
-  console.log('Mined -- ', waveTxn.hash);
+  const waveTxn: any = await wavePortalContract.wave(message, {
+    gasLimit: 300000
+  })
+  console.log('Mining...', waveTxn.hash)
+  await waveTxn.wait()
+  console.log('Mined -- ', waveTxn.hash)
 
   count = await wavePortalContract.getTotalWaves()
-  console.log('Retrieved total wave count', count.toNumber());
+  console.log('Retrieved total wave count', count.toNumber())
 }
 
 export const getAllWaves = async (): Promise<WaveMessage[]> => {
-  const wavePortalContract = await getPortalContract();
+  const wavePortalContract = await getPortalContract()
 
-  let waves: any[] = await wavePortalContract.getAllWaves();
+  let waves: any[] = await wavePortalContract.getAllWaves()
 
-  const wavesCleaned: WaveMessage[] = [];
+  const wavesCleaned: WaveMessage[] = []
 
   waves.forEach(wave => {
     wavesCleaned.push({
@@ -57,20 +60,25 @@ export const getAllWaves = async (): Promise<WaveMessage[]> => {
       timestamp: new Date(wave.timestamp * 1000),
       message: wave.message
     })
-  });
+  })
 
-  await listenToWavePortal();
+  await listenToWavePortal()
 
-  return wavesCleaned;
-
+  return wavesCleaned
 }
 
 const listenToWavePortal = async () => {
-  const wavePortalContract = await getPortalContract();
+  const wavePortalContract = await getPortalContract()
 
-  wavePortalContract.on("winnerWinner", (from, time) => {
-    console.log("We got a winner!", from, time)
+  wavePortalContract.on('winnerWinner', (from, time) => {
+    console.log('We got a winner!', from, time)
     const date = new Date(time * 1000)
-    alert("A winner has been selected! They are: " + from + " and they won at " + date + " give it up for them!");
+    alert(
+      'A winner has been selected! They are: ' +
+        from +
+        ' and they won at ' +
+        date +
+        ' give it up for them!'
+    )
   })
 }
